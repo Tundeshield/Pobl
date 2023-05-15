d4b6be2158ea5080d93be1d12c95f97b1ec9b210
Tunde Pegba <tundeshield@gmail.com>
2023-05-15 04:14:19 +0100

    Add button component

--- new-user-requests/src/scenes/StarterRequests.jsx
+++ new-user-requests/src/scenes/StarterRequests.jsx
@@ -1,9 +1,10 @@
+import FilterContainer from "../components/FilterContainer";
 import StarterRequestCard from "../components/StarterRequestCard";
 
 const StarterRequests = () => {
   return (
-    <div className="md:flex justify-between w-3/5  mt-8 p-2">
-      <div className="w-full h-full p-2 bg-gray-100 rounded-lg">
+    <div className="md:flex justify-between w-4/5  mt-8 p-2">
+      <div className="w-4/5 h-full p-2 bg-gray-100 rounded-lg">
         <div className="my-4">
           <h4 className="text-lg font-bold text-gray-700">
             View 40 starter requests.
@@ -14,7 +15,9 @@ const StarterRequests = () => {
         <StarterRequestCard />
         <StarterRequestCard />
       </div>
-      <div className="flex-auto w-32">fILTER CONTAINER</div>
+      <div className="flex-auto w-32">
+        <FilterContainer />
+      </div>
     </div>
   );
 };
