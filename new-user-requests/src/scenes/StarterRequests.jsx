d2573545cf05646744e4246bae98cdfe2937a36d
Tunde Pegba <tundeshield@gmail.com>
2023-05-15 01:09:02 +0100

    starter request card

--- new-user-requests/src/scenes/StarterRequests.jsx
+++ new-user-requests/src/scenes/StarterRequests.jsx
@@ -6,14 +6,20 @@ const StarterRequests = () => {
     <div className="md:flex justify-between w-4/5 h-4/5 mt-8 p-4">
       <div className="w-4/5 p-4 bg-gray-100 rounded-lg">
         <h4>View 40 starter requests.</h4>
-        <div className="w-4/5 p-10 bg-white m-auto mb-8 rounded-lg shadow-md">
-          hello
-        </div>
+
         <div className="w-4/5 p-2 bg-white m-auto mb-8 rounded-lg shadow-md">
-          hello
-        </div>
-        <div className="w-4/5 p-10 bg-white m-auto mb-8 rounded-lg shadow-md">
-          hello
+          <span className="flex ml-2">
+            <h2 className="mr-2 font-semibold">Full Name:</h2>
+            <p>Tunde Adepegba</p>
+          </span>
+          <span className="flex ml-2">
+            <h2 className="mr-2 font-semibold">Line Manager:</h2>
+            <p>Bolajko</p>
+          </span>
+          <span className="flex ml-2">
+            <h2 className="mr-2 font-semibold">First Name:</h2>
+            <p>Tunde Adepegba</p>
+          </span>
         </div>
       </div>
       <div className="flex-auto w-32">fILTER CONTAINER</div>
